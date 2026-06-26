4fdb5cdd87b672f3b4465c921b2786fc5ba4a2d7
thanusha-stack <thanusha13062006@gmail.com>
2026-06-26 18:29:28 +0530

    push sub folders to week1

---
+++ LibraryManagement/src/main/java/com/library/aspect/LoggingAspect.java
@@ -0,0 +1,27 @@
+package com.library.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class LoggingAspect {
+
+    @Around("execution(* com.library.service.*.*(..))")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        long start = System.currentTimeMillis();
+
+        Object result = joinPoint.proceed();
+
+        long end = System.currentTimeMillis();
+
+        System.out.println(
+                joinPoint.getSignature().getName()
+                        + " executed in "
+                        + (end - start)
+                        + " ms");
+
+        return result;
+    }
+}
--- LibraryManagement/src/main/java/com/library/service/BookService.java
+++ LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -2,14 +2,25 @@ package com.library.service;
 
 import com.library.repository.BookRepository;
 
-public class BookService{
+public class BookService {
+
     private BookRepository bookRepository;
 
-    public void setBookRepository(BookRepository bookRepository){
-        this.bookRepository=bookRepository;
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
     }
-    public void displayService(){
+
+    public void displayService() {
+
         System.out.println("BookService Bean Created");
+
+        bookRepository.displayRepository();
+
+        // delay so execution time is visible
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
-
