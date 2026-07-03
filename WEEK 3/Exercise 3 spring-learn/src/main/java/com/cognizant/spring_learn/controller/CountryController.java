54f852719c4019bedbb2a54fddb2742f57714dfb
thanusha-stack <thanusha13062006@gmail.com>
2026-07-03 19:18:14 +0530

    exercise 3 completed

---
+++ WEEK 3/Exercise 3 spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
@@ -0,0 +1,26 @@
+package com.cognizant.spring_learn.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.spring_learn.model.Country;
+
+@RestController
+public class CountryController {
+	private static final Logger LOGGER =
+            LoggerFactory.getLogger(CountryController.class);
+	@RequestMapping("/country")
+	public Country getCountryIndia() {
+		LOGGER.info("START");
+		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("country.xml");
+		Country country=context.getBean("country",Country.class);
+		LOGGER.info("END");
+
+        context.close();
+
+        return country;
+	}
+}
